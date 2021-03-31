e5efc1cac310e64c0991612bb975fc0acfd808f7
yash boura <yashboura303@gmail.com>
2021-03-31 18:20:51 +0530

    .gitignore Fixed

--- client/src/components/SupplyChain/Supplychain.js
+++ client/src/components/SupplyChain/Supplychain.js
@@ -2,20 +2,27 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 export default function Supplychain(props) {
     const { batchNo } = useParams();
-    const [details, setDetails] = useState();
+    // const [details, setDetails] = useState();
     const getDetails = () => {
         props.getSupplyChainDetails(batchNo);
     };
-    useEffect(() => {
-        getDetails();
-        console.log('first render');
-        console.log(props);
-    }, []);
+    // useEffect(() => {
+    //     getDetails();
+    //     console.log('first render');
+    //     console.log(props);
+    // }, []);
     return (
         <div>
             <h3 className="text-center my-4">
                 SupplyChain with Batch No as {batchNo}
             </h3>
+            <button
+                onClick={() => {
+                    props.getSupplyChainDetails(batchNo);
+                }}
+            >
+                Click
+            </button>
             {props.details && (
                 <p className="text-capitalize">
                     {props.details.registrationNo} || {props.details.farmerName}{' '}
